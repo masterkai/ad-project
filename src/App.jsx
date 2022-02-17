60888eb7ffecf161e386811ecc377e4187b5858d
masterkai <kai0919@hotmail.com>
2022-02-17 17:01:02 +0800

    魔都富二代 commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ class App extends Component {
     return (
       <div className="box">
         Hello, React.
-        <span>I am Jack.</span>
+        <span>I am Jack. I am a React dev：）</span>
       </div>
     );
   }
