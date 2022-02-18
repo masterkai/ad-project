c3609e56610639bec8c52fd3381491d79427f2b6
masterkai <kai0919@hotmail.com>
2022-02-18 15:12:35 +0800

    add eslint configuration setting

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './style.scss';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="box">
-        Hello, React.
-        <span>I am Jack. I am a React dev：）</span>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="box">
+      Hello, React.
+      <span>I am Jack. I am a React dev：）</span>
+    </div>
+  );
 }
 
 export default App;
